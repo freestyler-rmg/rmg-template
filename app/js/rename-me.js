8a79f93da0db5a46766f8ce4523396df0dc560ef
Roy Margasa <roymargasa@Bug-Stomper.local>
2018-05-03 17:38:51 +0700

    global namespace protection

--- app/js/rename-me.js
+++ app/js/rename-me.js
@@ -1,24 +1,26 @@
-$(document).ready(function(){
+(function (global) {
+	$(document).ready(function(){
 
 
-	// --- RESPONSIVE JS ---------------------------
-	// media query event handler
-	if (matchMedia) {
-		var mq = window.matchMedia("(min-width: 767px)");
-		mq.addListener(WidthChange);
-		WidthChange(mq);
-	}
-
-	// media query change
-	function WidthChange(mq) {
-
-		if (mq.matches) {
-			// window width is at least 767px
-		}
-		else {
-			// window width is less than 767px
+		// --- RESPONSIVE JS ---------------------------
+		// media query event handler
+		if (matchMedia) {
+			var mq = window.matchMedia("(min-width: 767px)");
+			mq.addListener(WidthChange);
+			WidthChange(mq);
 		}
 
-	}
+		// media query change
+		function WidthChange(mq) {
 
-});
+			if (mq.matches) {
+				// window width is at least 767px
+			}
+			else {
+				// window width is less than 767px
+			}
+
+		}
+
+	});
+})(window);
